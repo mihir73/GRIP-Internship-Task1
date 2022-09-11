b428601b3a521b5c071e29bfdd991899733c8358
mihir73 <chavdamihirsinh@gmail.com>
2022-09-11 18:42:04 +0530

    first commit

---
+++ mydb.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 08, 2021 at 06:52 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `sr` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `message` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`sr`, `name`, `email`, `message`) VALUES
+(13, 'Mihir chavda', 'mihir.official07@g', 'What is the python course fee?'),
+(14, 'mihir', 'cdef27953@gmail.com', 'This is a demo paragraph!'),
+(15, 'mihir', 'cdef27095@gmail.com', 'This is a para!'),
+(16, '', '', ''),
+(17, '', '', ''),
+(18, '', '', ''),
+(19, 'Chavda mihir', 'msd@gmail.com', 'this is my new contact email');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `sr` int(11) NOT NULL,
+  `sender` varchar(10) NOT NULL,
+  `receiver` varchar(10) NOT NULL,
+  `amount` bigint(20) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`sr`, `sender`, `receiver`, `amount`, `status`) VALUES
+(1, '7311780855', '5353112532', 200, 'succeed'),
+(2, '', '', 0, 'failed'),
+(3, '', '', 0, 'failed'),
+(4, '', '', 0, 'failed'),
+(5, '', '', 0, 'failed'),
+(6, '7311780855', '5353112532', 100, 'succeed'),
+(7, '7311780855', '5353112532', 0, 'failed'),
+(8, '7311780855', '4001286186', 200, 'succeed'),
+(9, '7311780855', '4001286186', 500, 'succeed'),
+(10, '3917416800', '4001286186', 14000, 'failed'),
+(11, '', '', 0, 'failed'),
+(12, '7311780855', '4001286186', 99500, 'succeed'),
+(13, '4001286186', '7311780855', 500, 'succeed'),
+(14, '4001286186', '5353112532', 800, 'succeed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `sr` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `accno` varchar(10) NOT NULL,
+  `blc` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`sr`, `name`, `email`, `accno`, `blc`) VALUES
+(3, 'mihir chavda', 'mihir@gmail.com', '5353112532', 321100),
+(4, 'prathamesh devalalikar ', 'pratham@gmail.com', '7311780855', 323000),
+(5, 'dhyan patel', 'dhyan@gmail.com', '4001286186', 100100),
+(6, 'darshan gajera', 'darshan@gmail.com', '1097481093', 162000),
+(7, 'deep patel', 'deep@gmail.com', '5859722581', 182000),
+(8, 'prashant pandey', 'prashant@gmail.com', '4920438199', 200000),
+(9, 'akshay jiwnani', 'akshay@gmail.com', '9304807119', 156100),
+(10, 'dhruv mandora', 'dhruv@gmail.com', '1384625500', 610900),
+(11, 'ayush joshi', 'ayush@gmail.com', '3917416800', 213900),
+(12, 'anand patel', 'anand@gmail.com', '5002694111', 199000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
+  ADD PRIMARY KEY (`sr`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`sr`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`sr`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
